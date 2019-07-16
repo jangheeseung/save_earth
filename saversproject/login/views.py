7ff0f6aaa3bab962b0d76aba5f0121b281720bd4
jangheeseung <gmltmd121@gmail.com>
2019-07-16 21:04:14 +0900

    login or signup

--- saversproject/login/views.py
+++ saversproject/login/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import get_object_or_404, render, redirect
 # Create your views here.
 def login(request):
         return render(request,'login/login.html')
+
+def signup(request):
+        return render(request,'login/signup.html')
