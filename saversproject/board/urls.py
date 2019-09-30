daf876b6d64db80f1e24c1f26daa5e00fc816dd1
jangheeseung <gmltmd121@gmail.com>
2019-09-30 19:50:12 +0900

    product menu modify board

--- saversproject/board/urls.py
+++ saversproject/board/urls.py
@@ -5,9 +5,9 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('',views.board,name="board"),
+    path('n_board/',views.n_board,name="n_board"),
+    path('q_board/',views.q_board,name="q_board"),
     path('write/',views.write,name="write"),
-    path('detail/',views.detail,name="detail"),
-
+    path('n_detail/',views.n_detail,name="n_detail"),
+    path('q_detail/',views.q_detail,name="q_detail"),
 ]
-
--- saversproject/board/views.py
+++ saversproject/board/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import get_object_or_404, render, redirect
 
 # Create your views here.
-def board(request):
-        return render(request,'board/board.html')
+def n_board(request):
+        return render(request,'board/n_board.html')
+def q_board(request):
+        return render(request,'board/q_board.html')
 def write(request):
         return render(request,'board/write.html')
-def detail(request):
-        return render(request,'board/detail.html')
+def n_detail(request):
+        return render(request,'board/n_detail.html')
+def q_detail(request):
+        return render(request,'board/q_detail.html')
